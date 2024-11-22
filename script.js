f49be4d84a4498040c584927c1d6cc9dcf79b5bf
Jeff <jeffhartley16@gmail.com>
2024-11-22 16:36:55 -0500

    Added JavaScript for part 1

---
+++ script.js
@@ -0,0 +1,23 @@
+//variable for the student object
+let student = {
+    name: "Jeff Hartley",
+    age: 31,
+    enrolled: true,
+    courses: ["Fundamentals of JavaScript", "Web Programming and Design", "Database Concepts & Design", "Algorithms & Problem Solving", "Operating Systems", "Technical Reading and Writing Skills"],
+}
+
+//function to display info of the student
+let displayInfo = () => {
+    return `Name of student: ${student.name}
+    Age of student: ${student.age}
+    Are they enrolled? ${student.enrolled}
+    The students courses: ${student.courses}`;
+}
+
+//logging required outputs to the console
+console.log(student.name);
+console.log(student.age);
+console.log(" "); // creates a gap in console for better reading
+console.log(displayInfo(student));
+
+
