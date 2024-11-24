85069185274621203b95652d162a66718ceac5bf
Jeff <jeffhartley16@gmail.com>
2024-11-23 23:09:17 -0500

    Used destructuring for the lab

--- script.js
+++ script.js
@@ -27,7 +27,18 @@ let jsonStudent = JSON.stringify(student);
 console.log(jsonStudent);
 console.log(" "); // creates a gap in console for better reading
 
-//converting the JSON string back to JS
+// converting the JSON string back to JS
 let convertedStudent = JSON.parse(jsonStudent);
 
 console.log(convertedStudent);
+console.log(" "); // creates a gap in console for better reading
+
+
+// extracting name and courses from the student object through destructuring
+let { name, courses } = student; // instruction didn't ask for anything done with this other that destructuring
+
+//creating an array and destructuring the first two scores
+let scores = [76, 90, 87, 99, 67];
+let [firstScore, secondScore] = scores;
+
+console.log(`First score destructured: ${firstScore}. Second score destructured: ${secondScore}`);
