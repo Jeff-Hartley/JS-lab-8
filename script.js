40ac3d619b4933dbf36bdb102546297d86e11eba
Jeff <jeffhartley16@gmail.com>
2024-11-23 22:45:55 -0500

    Worked with JSON

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let student = {
     enrolled: true,
     courses: ["Fundamentals of JavaScript", "Web Programming and Design", "Database Concepts & Design", "Algorithms & Problem Solving", "Operating Systems", "Technical Reading and Writing Skills"],
 
+    //method to display info
     displayInfo: function() {
         return `Name of student: ${student.name}
         Age of student: ${student.age}
@@ -18,5 +19,15 @@ console.log(student.name);
 console.log(student.age);
 console.log(" "); // creates a gap in console for better reading
 console.log(student.displayInfo());
+console.log(" "); // creates a gap in console for better reading
 
+// converting student object into JSON string
+let jsonStudent = JSON.stringify(student);
 
+console.log(jsonStudent);
+console.log(" "); // creates a gap in console for better reading
+
+//converting the JSON string back to JS
+let convertedStudent = JSON.parse(jsonStudent);
+
+console.log(convertedStudent);
